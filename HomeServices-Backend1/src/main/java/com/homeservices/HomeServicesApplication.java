7d5d623cf269e5ec4bd637152a997088489b7526
2300030055 <navadeepavanigadda@gmail.com>
2025-09-23 10:35:01 +0530

    badskm

--- HomeServices-Backend1/src/main/java/com/homeservices/HomeServicesApplication.java
+++ HomeServices-Backend1/src/main/java/com/homeservices/HomeServicesApplication.java
@@ -2,9 +2,10 @@ package com.homeservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HomeServicesApplication {
+public class HomeServicesApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(HomeServicesApplication.class, args);
     }
